37f67307f3f1b8f2d53298559e8bab72c8200b7d
rikic <rikicapoeirista3@gmail.com>
2022-06-12 23:50:48 +0800

    first

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Kuliah\SMT-6\Komputasi Paralel dan Terdistribusi\WEB\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
